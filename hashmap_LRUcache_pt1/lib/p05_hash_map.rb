62cf079ebfc71fdb32a3e803682553f2a8a85b7c
sam <s.gelernter@gmail.com>
2021-11-01 17:06:54 -0400

    Begin hash map

--- hashmap_LRUcache_pt1/lib/p05_hash_map.rb
+++ hashmap_LRUcache_pt1/lib/p05_hash_map.rb
@@ -1,20 +1,27 @@
 require_relative 'p04_linked_list'
 
 class HashMap
+
+  include Enumerable
+  
   attr_accessor :count
+  attr_reader :buckets
 
   def initialize(num_buckets = 8)
     @store = Array.new(num_buckets) { LinkedList.new }
     @count = 0
+    @buckets = num_buckets
   end
 
   def include?(key)
   end
 
   def set(key, val)
+    bucket(key).append(key, val)
   end
 
   def get(key)
+    bucket(key)
   end
 
   def delete(key)
@@ -23,13 +30,12 @@ class HashMap
   def each
   end
 
-  # uncomment when you have Enumerable included
-  # def to_s
-  #   pairs = inject([]) do |strs, (k, v)|
-  #     strs << "#{k.to_s} => #{v.to_s}"
-  #   end
-  #   "{\n" + pairs.join(",\n") + "\n}"
-  # end
+  def to_s
+    pairs = inject([]) do |strs, (k, v)|
+      strs << "#{k.to_s} => #{v.to_s}"
+    end
+    "{\n" + pairs.join(",\n") + "\n}"
+  end
 
   alias_method :[], :get
   alias_method :[]=, :set
@@ -44,6 +50,8 @@ class HashMap
   end
 
   def bucket(key)
-    # optional but useful; return the bucket corresponding to `key`
+    hash = key.hash
+    @store[hash % buckets]
   end
+  
 end
